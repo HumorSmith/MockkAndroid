bd62bed647b52d20e9f14f9ae7ce3add0c719857
wuyihua86 <wuyihua86@gmail.com>
2022-03-06 21:14:35 +0800

    增加登录的Expresso测试

--- app/src/androidTest/java/com/ifreedomer/mockk/TestLoginActivityWrong.kt
+++ app/src/androidTest/java/com/ifreedomer/mockk/TestLoginActivityWrong.kt
@@ -19,51 +19,31 @@ import org.hamcrest.Matchers.`is`
 import org.junit.Before
 import org.junit.After
 import org.junit.Rule
+
+
 @RunWith(AndroidJUnit4::class)
 @LargeTest
-class TestLoginActivity {
-    var idleResource: SimpleIdlingResource? = null
-
-
+class TestLoginActivityWrong {
     @get:Rule
     var activityScenario = ActivityScenarioRule(LoginActivity::class.java)
 
-    @Before
-    fun setup() {
-        activityScenario.scenario.onActivity {
-            val activity = it as LoginActivity
-            idleResource = activity.idleResource
-            // To prove that the test fails, omit this call:
-            IdlingRegistry.getInstance().register(activity.idleResource)
-        }
-    }
-
     @Test
-    fun testLoginSuccess() {
+    fun testLoginSuccessWrong1() {
         //withId withText  withResourceName  withTagKey  withTagValue
         onView(withId(R.id.username_et)).perform(click()).perform(typeText("Engineer"))
         onView(withId(R.id.passowrd_et)).perform(click()).perform(typeText("Engineer"))
-        //一定要收起键盘，否则会找不到登录按钮。不要问我为什么知道
-        closeSoftKeyboard()
         onView(withId(R.id.login_btn)).perform(click())
         onView(withTagValue(`is`(LoginActivity.LOGIN_SUCCESS_TAG))).check(matches(isDisplayed()))
     }
 
+
     @Test
-    fun testLoginFailed() {
-        onView(withId(R.id.username_et)).perform(click()).perform(typeText(""))
+    fun testLoginSuccessWrong2() {//偶现case错误，因为未处理异步逻辑
+        //withId withText  withResourceName  withTagKey  withTagValue
+        onView(withId(R.id.username_et)).perform(click()).perform(typeText("Engineer"))
         onView(withId(R.id.passowrd_et)).perform(click()).perform(typeText("Engineer"))
         closeSoftKeyboard()
-        //一定要收起键盘，否则会找不到登录按钮
         onView(withId(R.id.login_btn)).perform(click())
-        onView(withTagValue(`is`(LoginActivity.LOGIN_FAILED_TAG))).check(matches(isDisplayed()))
+        onView(withTagValue(`is`(LoginActivity.LOGIN_SUCCESS_TAG))).check(matches(isDisplayed()))
     }
-
-    @After
-    fun release() {
-        if (idleResource != null) {
-            IdlingRegistry.getInstance().unregister(idleResource)
-        }
-    }
-
 }
